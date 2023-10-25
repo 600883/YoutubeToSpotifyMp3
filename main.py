a03862fa9b4ba331b6cb9243ef441bad13e3a9b6
sivertsaeter <sivertseter@yahoo.com>
2023-10-25 22:04:27 +0200

    removed some doc

--- main.py
+++ main.py
@@ -8,12 +8,7 @@ link = YouTube(str(input("Enter URL av youtube videoen du vil laste ned: \n>>"))
 # extract only audio
 video = link.streams.filter(only_audio=True).first()
 
-# check for destination to save file
-# print("Hvilken mappe vil du lagre filen i (la være blank for gjeldene mappe)")
-# destination = str(input(">> ")) or '.'
-
 # specify which folder on your mac, that already exists
-# destination = 'Users/sivertsaeter/Documents/NotOnSpotify/'
 destination = os.path.dirname('/Users/sivertsaeter/Documents/NotOnSpotify/')
 
 # download the file 
